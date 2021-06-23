f7ed9e520de44b82b3cb38a39a615a2ae2ba46e3
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 20:22:02 +0600

    user delete, create, update functionality reated done

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Post = require('../models/Post');
 const bcrypt = require('bcrypt');
+const { route } = require('./auth');
 
 
 //UPDATE
@@ -29,6 +31,35 @@ router.put("/:id", async (req, res) => {
 
 
 //DELETE
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({ username: user.username })
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("User has been deleted");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } catch (err) {
+            res.status(404).json("User not found")
+        }
+    } else {
+        res.status(401).json("You can delete only your account!");
+    }
+});
+
+//GET USER
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...rest } = user._doc;
+        res.status(200).json(rest)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router;
