ea33c467d97f3b26a35d1e0aab4e85061105447f
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 13:47:30 +0600

    post schema created

---
+++ models/Post.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: false
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    categories: {
+        type: Array, 
+        required: true
+    }
+}, {
+    timeStamps: true
+}
+)
+
+module.exports = mongoose.modal('Post', PostSchema)
