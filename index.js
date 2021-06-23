830743e0a00ee6376953828999217b263653e325
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 23:22:48 +0600

    multer set up complete

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 const categoryRoute = require('./routes/categories');
+const multer = require('multer')
 
 dotenv.config();
 app.use(express.json());
@@ -19,12 +20,25 @@ mongoose
         useCreateIndex: true
     })
     .then(console.log('Connect to mongoDB'))
-    .then(err => console.log(err))
+    .catch(err => console.log(err))
 
-    app.use("/api/auth", authRoute)
-    app.use('/api/users', userRoute)
-    app.use('/api/posts', postRoute)
-    app.use('/api/categories', categoryRoute)
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    }, filename: (req, file, callback) => {
+        callback(null, req.body.name)
+    }
+})
+
+const upload = multer({ storage: storage })
+app.post('/api/upload', upload.single("file"), (req, res) => {
+    res.status(200).json('File has been uploaded')
+})
+
+app.use("/api/auth", authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
+app.use('/api/categories', categoryRoute)
 
 app.listen('5000', () => {
     console.log('Backend is running');
