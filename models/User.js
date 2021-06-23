a3967a991fbd8bc924b65bc0e949ada632eaa346
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 12:43:40 +0600

    use schema created done

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+},
+    {
+        timeStamps: true
+    }
+)
+
+module.exports = mongoose.modal('User', UserSchema)
