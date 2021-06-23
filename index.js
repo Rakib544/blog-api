359847204ba7b090d984971ade56a1ad7aef5453
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 12:33:16 +0600

    some folder structure created

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(console.log('Connect to mongoDB'))
+    .then(err => console.log(err))
 
 app.listen('5000', () => {
     console.log('Backend is running');
