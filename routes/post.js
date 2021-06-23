9aca1fd00c9b52895c5ab0e90c1141f6eac1148a
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 20:52:20 +0600

    update post functionality done

--- routes/post.js
+++ routes/post.js
@@ -11,7 +11,7 @@ router.post("/", async (req, res) => {
     try {
         const savedPost = await newPost.save();
         res.status(200).json(savedPost)
-    }catch(err) {
+    } catch (err) {
         res.status(500).json(err)
     }
 
@@ -19,31 +19,42 @@ router.post("/", async (req, res) => {
 
 
 //UPDATE POST
-router.delete("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id) {
-        try {
-            const user = await User.findById(req.params.id);
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.username === req.body.username) {
             try {
-                await Post.deleteMany({ username: user.username })
-                await User.findByIdAndDelete(req.params.id)
-                res.status(200).json("User has been deleted");
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                }, { new: true })
+                res.status(200).json(updatedPost)
             } catch (err) {
-                res.status(500).json(err);
+                res.status(500).json(err)
             }
-        } catch (err) {
-            res.status(404).json("User not found")
+        } else {
+            res.status(401).json('You can update only your post')
         }
-    } else {
-        res.status(401).json("You can delete only your account!");
+    } catch (err) {
+        res.status(500).json(err)
     }
 });
 
 //DELETE POST
 router.get('/:id', async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        const { password, ...rest } = user._doc;
-        res.status(200).json(rest)
+        const post = await Post.findById(req.params.id)
+        if (post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                }, { new: true })
+                res.status(200).json(updatedPost)
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        } else {
+            res.status(401).json('You can delete only your post')
+        }
     } catch (err) {
         res.status(500).json(err)
     }
