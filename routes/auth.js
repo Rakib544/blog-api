93e95423b2197518ce18c1bc2bae1d936342198b
Rakib544 <md.rakib10122003@gmail.com>
2021-06-23 16:26:33 +0600

    register functinality done

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 
 //REGISTER
 router.post('/register', async (req, res) => {
     try {
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt)
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPass
         });
 
         const user = await newUser.save();
