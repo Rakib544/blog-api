9eb2c0da18bfee89e0d5700fa7321836857d644c
Rakib544 <md.rakib10122003@gmail.com>
2021-06-26 16:09:39 +0600

    some code updated

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,8 @@ router.post('/register', async (req, res) => {
         });
 
         const user = await newUser.save();
-        res.status(200).json(user)
+        const {...rest, password} = user;
+        res.status(200).json(rest)
 
     } catch (err) {
         res.status(500).json(err)
